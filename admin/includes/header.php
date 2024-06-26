d6c1435a0796050ab5a9e2f5c0eb3f5e512d5fba
HenryBonney <henrybonney92@gmail.com>
2024-06-26 12:00:23 +0000

    second updt

---
+++ admin/includes/header.php
@@ -0,0 +1,15 @@
+<div class="header">
+        <div class="nav">
+           
+            <span></span>
+            <h2>Dashboard</h2>
+            
+            <div class="search">
+                <i class="fa-solid fa-search"></i>
+                    <input type="text" placeholder="Search...">
+         
+            </div>
+            <img src="<?php echo $base_url; ?>/images/user.png" alt=""/>
+
+            
+        </div>
---
+++ includes/side_bar.php
@@ -0,0 +1,97 @@
+
+<!--Side bar-->
+<div class="sidebar">
+    <!--logo on sidebar-->
+    <img src="https://i.ibb.co/JdQWY17/ki-logo.png" alt="ki_Logo" class="logo">
+
+   <!--Dashboard button-->
+    <ul class="menu-item ">
+        <li class="active">
+            <a href="adminDashboard.php">
+                <i class="fas fa-tachometer-alt"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+    </ul>
+    
+    <!--Educators button-->
+    <ul class="menu-item ">
+        <li>
+            <a href="educators.php">
+                <i class="fas fa-chalkboard-teacher"></i>
+                <span>Educators</span>
+            </a>
+        </li>
+    </ul>
+
+     <!--SEL Themes button-->
+     <ul class="menu-item ">
+        <li>
+            <a href="#">
+                <i class="fas fa-book"></i>
+                <span>SEL Themes</span>
+            </a>
+        </li>
+    </ul>
+
+     <!--Schools button-->
+     <ul class="menu-item ">
+        <li>
+            <a href="school.php">
+                <i class="fas fa-school"></i>
+                <span>Schools</span>
+            </a>
+        </li>
+    </ul>
+
+     <!--Students button-->
+     <ul class="menu-item ">
+        <li>
+            <a href="student.php">
+                <i class="fas fa-user-graduate"></i>
+                <span>Students</span>
+            </a>
+        </li>
+    </ul>
+
+     <!--Reports button-->
+     <ul class="menu-item ">
+        <li>
+            <a href="report.php">
+                <i class="fas fa-file-alt"></i>
+                <span>Reports</span>
+            </a>
+        </li>
+    </ul>
+
+     <!--Assign role button-->
+     <ul class="menu-item ">
+        <li>
+            <a href="manageuser.php">
+                <i class="fas fa-user-cog"></i>
+                <span>Assign Role</span>
+            </a>
+        </li>
+    </ul>
+
+     <!--Reports button-->
+     <ul class="menu-item ">
+        <li>
+            <a href="settings.php">
+                <i class="fas fa-cog"></i>
+                <span>Settings</span>
+            </a>
+        </li>
+    </ul>
+
+     <!--Logout button-->
+     <ul class="menu-item">
+    <li>
+        <a href="logout.php">
+            <i class="fas fa-sign-out-alt"></i>
+            <span>Log Out</span>
+        </a>
+    </li>
+</ul>
+      
+</div>
